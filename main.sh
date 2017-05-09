8271e3fa1d2b74d81ea0b5b967a04f5c50629471
rn4ir <rn@rahul-nair.com>
2017-05-09 01:42:16 -0400

    added dependencies and completed ffmpeg install

--- main.sh
+++ main.sh
@@ -43,3 +43,63 @@ make
 make install
 
 
+cd /opt/ffmpeg-sources/
+git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
+cd fdk-aac
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+cd /opt/ffmpeg-sources/
+curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+tar xzvf lame-3.99.5.tar.gz
+cd lame-3.99.5
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
+make
+make install
+
+
+cd /opt/ffmpeg-sources/
+wget http://downloads.xiph.org/releases/opus/opus-1.1.4.tar.gz
+tar zxvf opus-1.1.4.tar.gz
+cd opus-1.1.4
+autoreconf -fiv
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+
+cd /opt/ffmpeg-sources/
+curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+tar xzvf libogg-1.3.2.tar.gz
+cd libogg-1.3.2
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+cd /opt/ffmpeg-sources/
+curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
+tar xzvf libvorbis-1.3.4.tar.gz
+cd libvorbis-1.3.4
+./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+
+cd /opt/ffmpeg-sources/
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
+cd libvpx
+./configure --prefix="$HOME/ffmpeg_build" --disable-examples
+make
+make install
+
+
+cd /opt/ffmpeg-sources/
+curl -O http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib -ldl" --bindir="$HOME/bin" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265
+make
+make install
+hash -r
